5c1fe86be78215f72197684f1562a658a9081b13
Enes UTKU <ensutku@hotmail.com>
2024-05-24 17:51:14 +0300

    SwiftUI Default Project

---
+++ notes-swiftui/notes-swiftui/notes_swiftuiApp.swift
@@ -0,0 +1,12 @@
+// Created by Enes UTKU
+
+import SwiftUI
+
+@main
+struct notes_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
