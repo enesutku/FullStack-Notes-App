0690d18251fa30c09304c61e7dcbc74d2cca1263
Enes UTKU <ensutku@hotmail.com>
2024-05-25 09:39:57 +0300

    Update

--- notes-swiftui/Note.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-struct Note: Identifiable, Codable {
-    var id: String { _id }
-    var _id: String
-    var note: String
-}
--- notes-swiftui/notes-swiftui/Root/notes_swiftuiApp.swift
+++ notes-swiftui/notes-swiftui/Root/notes_swiftuiApp.swift
@@ -6,7 +6,7 @@ import SwiftUI
 struct notes_swiftuiApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            Home()
         }
     }
 }
